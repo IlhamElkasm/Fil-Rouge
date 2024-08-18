b96cf7015dea43ecee1982024bb5245369011a81
ily-123 <elkasmiilham994@gmail.com>
2024-08-18 22:42:10 +0100

    structure de projet

---
+++ DreamCakes/src/main/java/com/cakes/DreamCakesApplication.java
@@ -0,0 +1,13 @@
+package com.cakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamCakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamCakesApplication.class, args);
+	}
+
+}
