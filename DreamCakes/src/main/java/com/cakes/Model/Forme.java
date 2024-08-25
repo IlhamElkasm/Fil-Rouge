17725b79e62f4d3f531c99a488770a861bc92edf
ily-123 <elkasmiilham994@gmail.com>
2024-08-25 21:26:47 +0100

    Create table Forme

---
+++ DreamCakes/src/main/java/com/cakes/Model/Forme.java
@@ -0,0 +1,22 @@
+package com.cakes.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Forme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idShape;
+    private String name;
+    private String dimensions;
+    private double price;
+}
