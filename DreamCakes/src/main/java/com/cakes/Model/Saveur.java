24a58f580fd8bb3f4f3efa2a5fda9ac0acf5416d
ily-123 <elkasmiilham994@gmail.com>
2024-08-26 11:50:03 +0100

    Create clsse Saveur

---
+++ DreamCakes/src/main/java/com/cakes/Model/Saveur.java
@@ -0,0 +1,23 @@
+package com.cakes.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Saveur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idFlavor;
+    private String name;
+    private double price;
+
+}
