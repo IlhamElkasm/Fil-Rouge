8d983a1e54562a4918090df19e6766f30ca691b8
ily-123 <elkasmiilham994@gmail.com>
2024-08-26 11:51:36 +0100

    Create interfece SaveurJpa

---
+++ DreamCakes/src/main/java/com/cakes/Repository/SaveurRepository.java
@@ -0,0 +1,7 @@
+package com.cakes.Repository;
+
+import com.cakes.Model.Saveur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaveurRepository extends JpaRepository<Saveur, Long> {
+}
