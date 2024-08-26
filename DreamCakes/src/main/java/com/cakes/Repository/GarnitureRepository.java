e17d697aa9c967a06651e17d2381896ca53c4112
ily-123 <elkasmiilham994@gmail.com>
2024-08-26 12:13:23 +0100

    Create interfece GarnitureJpa

---
+++ DreamCakes/src/main/java/com/cakes/Repository/GarnitureRepository.java
@@ -0,0 +1,7 @@
+package com.cakes.Repository;
+
+import com.cakes.Model.Garniture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GarnitureRepository extends JpaRepository<Garniture, Long> {
+}
