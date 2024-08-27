f7c75044d957e3669c51a798d79616c182263646
ily-123 <elkasmiilham994@gmail.com>
2024-08-27 23:31:29 +0100

    appliquer template avec angular

--- DreamCakesFront/src/app/app.module.ts
+++ DreamCakesFront/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { MainComponent } from './Components/main/main.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { FormComponent } from './Components/form/form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainComponent,
+    FooterComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
