7c5d2b25776fbb25ae3742c1af2b4a9f35a88c4e
ily-123 <elkasmiilham994@gmail.com>
2024-09-07 21:58:49 +0100

    create interface Repository  Commende

---
+++ DreamCakes/src/main/java/com/cakes/Repository/CommendeRepository.java
@@ -0,0 +1,7 @@
+package com.cakes.Repository;
+
+import com.cakes.Model.Commende;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommendeRepository extends JpaRepository<Commende, Integer> {
+}
