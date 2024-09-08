a3d7c2f35280417807688bfec01211212cf50590
ily-123 <elkasmiilham994@gmail.com>
2024-09-08 12:19:06 +0100

    create components login

---
+++ DreamCakesFront/src/app/Components/Authentication/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/Service/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+  role: string = '';
+
+  constructor(private authService: AuthenticationService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.email, this.password).subscribe(
+        (response) => {
+            localStorage.setItem('jwt', response.token);
+            localStorage.setItem('role', response.role);  // Store role
+            
+            if (response.role === 'ADMIN') {
+                this.router.navigate(['/dashboardA']);
+            } else if (response.role === 'USER') {
+                this.router.navigate(['/dashboard']);
+            } else {
+                alert('Rôle non reconnu');
+            }
+        },
+        (error) => {
+            alert('Échec de l\'authentification');
+        }
+    );
+}
+
+}
